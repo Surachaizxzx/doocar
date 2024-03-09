d491c191b85f1adcf7a2a8a95b16f34ad6af3aa8
Surachaizxzx <surachaikai2545@gmail.com>
2024-03-09 11:48:50 +0700

    e

--- lib/screen/hidden_drawer.dart
+++ lib/screen/hidden_drawer.dart
@@ -11,18 +11,6 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
-  Future Edit_Profile() async {
-    return;
-  }
-
-  Future Edit_Picture() async {
-    return;
-  }
-
-  Future Setting() async {
-    return;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -60,102 +48,10 @@ class _NavBarState extends State<NavBar> {
               ),
             ),
           ),
-          Column(
+          const Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5, 20, 70, 20),
-                child: Column(
-                  children: [
-                    TextButton.icon(
-                      onPressed: () {
-                        Edit_Profile();
-                      },
-                      style: TextButton.styleFrom(
-                        foregroundColor: const Color.fromARGB(255, 0, 0, 0),
-                        backgroundColor: Colors.white, // สีของตัวอักษรภายในปุ่ม
-                        elevation: 5, // ความสูงของเงา
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                            20,
-                          ), // การกำหนดรูปร่างของปุ่ม
-                        ),
-                      ),
-                      icon: const Icon(
-                        Icons.person_3_outlined,
-                        color: Color.fromARGB(255, 26, 25, 25),
-                      ),
-                      label: const Text(
-                        '\t\t\t\t\tEdit Profile\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t',
-                        style: TextStyle(
-                          fontFamily: 'CustomFont',
-                          fontSize: 16,
-                          fontWeight: FontWeight
-                              .normal, // This can be FontWeight.bold for the bold version
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextButton.icon(
-                      onPressed: () {
-                        Edit_Picture();
-                      },
-                      style: TextButton.styleFrom(
-                        foregroundColor: const Color.fromARGB(255, 0, 0, 0),
-                        backgroundColor: Colors.white, // สีของตัวอักษรภายในปุ่ม
-                        elevation: 5, // ความสูงของเงา
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              20), // การกำหนดรูปร่างของปุ่ม
-                        ),
-                      ),
-                      icon: const Icon(Icons.picture_as_pdf),
-                      label: const Text(
-                        "\t\t\t\t\tEdit Picture\t\t\t\t\t\t\t\t\t\t\t\t\t",
-                        style: TextStyle(
-                          fontFamily: 'CustomFont',
-                          fontSize: 16,
-                          fontWeight: FontWeight
-                              .normal, // This can be FontWeight.bold for the bold version
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextButton.icon(
-                      onPressed: () {
-                        Setting();
-                      },
-                      style: TextButton.styleFrom(
-                        foregroundColor: const Color.fromARGB(255, 0, 0, 0),
-                        backgroundColor: Colors.white, // สีของตัวอักษรภายในปุ่ม
-                        elevation: 5, // ความสูงของเงา
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              10), // การกำหนดรูปร่างของปุ่ม
-                        ),
-                      ),
-                      icon: const Icon(Icons.settings),
-                      label: const Text(
-                        "\t\t\t\tSetting\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",
-                        style: TextStyle(
-                          fontFamily: 'CustomFont',
-                          fontSize: 16,
-                          fontWeight: FontWeight
-                              .normal, // This can be FontWeight.bold for the bold version
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Login_logout_(),
-                  ],
-                ),
-              ),
+              Login_logout_(),
             ],
           ),
         ],
