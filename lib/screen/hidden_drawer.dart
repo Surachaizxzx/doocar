c3f90c694172fefc3be37fb9c0c2bc5e86c0bf7b
Surachaizxzx <surachaikai2545@gmail.com>
2024-03-15 19:11:14 +0700

    ำ
    ำ

--- lib/screen/hidden_drawer.dart
+++ lib/screen/hidden_drawer.dart
@@ -1,7 +1,11 @@
 import 'package:doocar/Login_logout.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:doocar/service/classuser.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'login.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
@@ -24,6 +28,20 @@ class _NavBarState extends State<NavBar> {
   @override
   void dispose() {
     super.dispose();
+  }
+
+  Future<void> _logout() async {
+    // ตั้งค่า isLoggedIn เป็น false และบันทึกลง SharedPreferences
+    await prefs.setBool('isLoggedIn', false);
+    removeSession();
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
+
+  void removeSession() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('session');
   }
 
   Future<String> getSession() async {
@@ -59,7 +77,6 @@ class _NavBarState extends State<NavBar> {
 
   Widget _loggedInView() {
     return Drawer(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       child: FutureBuilder(
         future: _usersFuture,
         builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
@@ -72,26 +89,116 @@ class _NavBarState extends State<NavBar> {
               itemCount: snapshot.data!.length,
               itemBuilder: (BuildContext context, int index) {
                 User user = snapshot.data![index];
-                return Column(
-                  children: [
-                    UserAccountsDrawerHeader(
-                      accountName: Text(user.username),
-                      accountEmail: Text(user.email),
-                      currentAccountPicture: CircleAvatar(
-                        child: ClipOval(
-                          child: Image.asset("assets/images/me.jpg"),
-                        ),
+                return Container(
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage("assets/images/blackground.jpg"),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    const Column(
+                                      children: [
+                                        Text(
+                                          "เมนู",
+                                          style: TextStyle(
+                                            fontSize: 30,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight
+                                                .bold, // น้ำหนักตัวอักษร
+                                            fontStyle: FontStyle.normal,
+                                            fontFamily: 'CustomFont',
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      width: 105,
+                                    ),
+                                    Column(
+                                      children: [
+                                        TextButton.icon(
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const LoginApp(),
+                                              ),
+                                            );
+                                            _logout();
+                                          },
+                                          icon: const Icon(
+                                            Icons.logout_outlined,
+                                            size: 40,
+                                            color: Colors.white,
+                                          ),
+                                          label: const Text(""),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 140,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 120),
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(70.0),
+                                ),
+                              ),
+                              child: const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      SizedBox(
+                                        width: 105,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 80,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      decoration: const BoxDecoration(
-                        color: Colors.pink,
-                        image: DecorationImage(
-                            image: AssetImage("assets/images/2.png"),
-                            fit: BoxFit.cover),
-                      ),
-                      currentAccountPictureSize: const Size(70, 70),
-                    ),
-                    const Login_logout_(),
-                  ],
+                      Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [Text(user.username)],
+                              ),
+                              Row(
+                                children: [Text(user.email)],
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
                 );
               },
             );
@@ -100,18 +207,116 @@ class _NavBarState extends State<NavBar> {
       ),
     );
   }
-}
 
-Widget _loggedOutView() {
-  return const Drawer(
-    child: Column(
-      children: [
-        UserAccountsDrawerHeader(
-          accountName: Text("visitors"),
-          accountEmail: Text("visitors@gmail.com"),
-        ),
-        Login_logout_(),
-      ],
-    ),
-  );
+  Widget _loggedOutView() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 22),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/blackground.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Column(
+                          children: [
+                            Text(
+                              "เมนู",
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold, // น้ำหนักตัวอักษร
+                                fontStyle: FontStyle.normal,
+                                fontFamily: 'CustomFont',
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 105,
+                        ),
+                        Column(
+                          children: [
+                            TextButton.icon(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginApp(),
+                                  ),
+                                );
+                                _logout();
+                              },
+                              icon: const Icon(
+                                Icons.logout_outlined,
+                                size: 40,
+                                color: Colors.white,
+                              ),
+                              label: const Text(""),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 140,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 120),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(70.0),
+                    ),
+                  ),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            width: 105,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 80,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: const Column(
+                children: [
+                  Row(
+                    children: [Text('Visitor Account')],
+                  ),
+                  Row(
+                    children: [Text('Visitor Email')],
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
+  }
 }
