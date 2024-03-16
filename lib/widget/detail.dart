292fe0f240e32fc3b6d6f01f8ca97ccd08b16298
Kittiphop22 <s6403051613172@email.kmutnb.ac.th>
2024-03-16 14:32:51 +0700

    Create detail.dart

---
+++ lib/widget/detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class detail extends StatelessWidget {
+  const detail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
