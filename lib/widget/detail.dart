2397735a7aae49b5b781cf4775ec3d91b8b3f1cc
Kittiphop22 <s6403051613172@email.kmutnb.ac.th>
2024-03-16 16:49:22 +0700

    Update detail.dart

--- lib/widget/detail.dart
+++ lib/widget/detail.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'slider.dart';
+
 
 class detail extends StatelessWidget {
   const detail({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title : const Text("Details"),
+        centerTitle:true,
+      ),
+      
+      body: SliderP(),
+    );
+    
   }
 }
