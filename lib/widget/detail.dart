1b2d09943ac04839b044476523290ac6c19d9314
Kittiphop22 <s6403051613172@email.kmutnb.ac.th>
2024-03-17 22:35:04 +0700

    Update detail.dart

--- lib/widget/detail.dart
+++ lib/widget/detail.dart
@@ -3,20 +3,56 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'slider.dart';
 
-
 class detail extends StatelessWidget {
   const detail({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title : const Text("Details"),
-        centerTitle:true,
-      ),
-      
-      body: SliderP(),
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              "ประเภทรถยนต์",
+              style: TextStyle(fontSize: 22, color: Colors.black,fontWeight: FontWeight.w900),
+            ),
+            SizedBox(
+          height: 10,
+        ),
+            Text(
+              "รถยนต์มือสอง",
+              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              "ราคา",
+              style: TextStyle(fontSize: 22, color: Colors.black,fontWeight: FontWeight.w900),
+            ),
+            SizedBox(
+          height: 10,
+        ),
+            Text(
+              "บาท",
+              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
     );
-    
   }
 }
