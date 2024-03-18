b86ebc2375a3437f2bce7822c188efdffdbcd66c
Kittiphop22 <s6403051613172@email.kmutnb.ac.th>
2024-03-18 16:10:21 +0700

    Update detail.dart

--- lib/widget/detail.dart
+++ lib/widget/detail.dart
@@ -1,5 +1,8 @@
+import 'package:doocar/component/my_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/widgets.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'slider.dart';
 
@@ -8,67 +11,159 @@ class detail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      children: [
+    return Column(
+      //mainAxisAlignment: MainAxisAlignment.start,
+      //mainAxisSize: MainAxisSize.max,
+      children:[
         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
           children: [
-            Text(
+        Container(
+          margin: EdgeInsets.fromLTRB(10,10,50,0),
+          padding: EdgeInsets.all(20.0),
+          child: 
+            const Text(
+              "2011 Audi R8 4.2 (ปี 06-15) FSI 4WD Coupe",
+              style: TextStyle(fontSize: 24, color: Colors.black,fontWeight: FontWeight.w900),
+            ),
+            //decoration: BoxDecoration(color: Colors.red),
+            
+        ),
+          ],
+        ),
+         const SizedBox(
+          height: 200,
+        ),
+        Row(
+          children: [
+            // Row(
+            //   children:[
+        Container(
+          margin: EdgeInsets.fromLTRB(10,10,50,0),
+          padding: EdgeInsets.all(20.0),
+          child: 
+            const Text(
               "ประเภทรถยนต์",
-              style: TextStyle(fontSize: 22, color: Colors.black,fontWeight: FontWeight.w900),
+              style: TextStyle(fontSize: 24, color: Colors.black,fontWeight: FontWeight.w900),
             ),
-            SizedBox(
-          width: 10,
+            //decoration: BoxDecoration(color: Colors.red),
         ),
-            Text(
-              "รถยนต์มือสอง",
-              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),
+            //   ]
+            // ),
+        
+            // Row(
+            //   children:[
+        Container(
+          margin: EdgeInsets.fromLTRB(10,10,50,0),
+          padding: EdgeInsets.all(20.0),
+              child: 
+            const Text(
+              "รถสปอร์ต",
+              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),              
             ),
-          ],
+            //decoration: BoxDecoration(color: Colors.blue),
+            ),
+        //   ],
+        // ),
+          ]
         ),
-        SizedBox(
-          width: 20,
+         const SizedBox(
+          height: 10,
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
           children: [
-            Text(
+            // Row(
+            //   children:[
+        Container(
+          margin: EdgeInsets.fromLTRB(10,10,50,0),
+          padding: EdgeInsets.all(20.0),
+          child: 
+            const Text(
               "ราคา",
-              style: TextStyle(fontSize: 22, color: Colors.black,fontWeight: FontWeight.w900),
+              style: TextStyle(fontSize: 24, color: Colors.black,fontWeight: FontWeight.w900),
             ),
-            SizedBox(
-          width: 10,
+            //decoration: BoxDecoration(color: Colors.red),
         ),
-            Text(
-              "บาท",
-              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),
+            //   ]
+            // ),
+        
+            // Row(
+            //   children:[
+        Container(
+          margin: EdgeInsets.fromLTRB(10,10,50,0),
+          padding: EdgeInsets.all(20.0),
+              child: 
+            const Text(
+              "4,250,000",
+              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),              
             ),
-          ],
+            //decoration: BoxDecoration(color: Colors.blue),
+            ),
+        //   ],
+        // ),
+          ]
         ),
         SizedBox(
-          width: 20,
+          height: 10,
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
+        Row(
           children: [
-            Text(
-              "ข้อมูลของรถยนต์",
-              style: TextStyle(fontSize: 22, color: Colors.black,fontWeight: FontWeight.w900),
+        Container(
+          margin: EdgeInsets.fromLTRB(10,10,50,0),
+          padding: EdgeInsets.all(20.0),
+          child: 
+            const Text(
+              "รายละเอียด",
+              style: TextStyle(fontSize: 24, color: Colors.black,fontWeight: FontWeight.w900),
             ),
-            SizedBox(
-          width: 10,
+            //decoration: BoxDecoration(color: Colors.red),
         ),
-            Text(
-              "รายละเอียดรถยนต์ database",
-              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),
-            ),
           ],
         ),
+        Row(
+          children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(10,10,50,10),
+          padding: EdgeInsets.all(20.0),
+              child: 
+            const Text(
+              "รถสวยจัดมาก ( ใช้เพียงแค่ 40,000 KM. ) เท่านั้น !!!เพราะ จะเอาไปขับแค่เฉพาะ วันอาทิตย์ เท่านั้นเป็นรุ่นพิเศษ ( Carbon-Edition ) แล้วกล้ายืนยัน สภาพใหม่จัด เหมือนรถป้ายแดง ของจริงแน่นอนไม่ต้องกังวลใจ เพราะ รถคันนี้ ถนอมแบบสุดหัวใจ จริงๆ",
+              style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.w600),              
+            ),
+            //decoration: BoxDecoration(color: Colors.blue),
+            ),
+        //   ],
+        // ),
+          ]
+        ),
+        Row(
+          children: [
+            Container(
+              child: Mybutton(onTap: () {
+                
+              }, hinText: "เพิ่มลงMycar",),
+            ),
+            Container(
+              child: Mybutton(onTap: () {}, hinText: "ซื้อ"),
+              //decoration: BoxDecoration(color: Colors.amber),
+            ),
+          ],
+            
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            Container(
+              child: Mybutton(onTap: () {
+                
+              }, hinText: "เพิ่มลงMycar",),
+            ),
+          ],
+            
+        ),
+        
+        
       ],
     );
   }
