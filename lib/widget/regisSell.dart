384b88fbaf93d342dc122a9e0f57fdf9247ca242
Kittiphop22 <s6403051613172@email.kmutnb.ac.th>
2024-03-18 23:06:46 +0700

    Update regisSell.dart

--- lib/widget/regisSell.dart
+++ lib/widget/regisSell.dart
@@ -1,1 +1,60 @@
-stw
+import 'package:doocar/component/my_textformfield.dart';
+import 'package:flutter/material.dart';
+
+class regisSell extends StatelessWidget {
+  regisSell({super.key});
+  final CarnameController = TextEditingController();
+  final PriceController = TextEditingController();
+  final TypeController = TextEditingController();
+  final InfoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Form(
+          child: Column(
+        children: [
+          Text(
+            "กรุณากรอกข้อมูลและรายละเอียดของรถยนต์",
+            style: TextStyle(
+                fontFamily: 'Customfont',
+                fontSize: 28,
+                fontWeight: FontWeight.w900),
+          ),
+          MytextField(
+                controller: CarnameController,
+                hintText: 'Enter your name',
+                obscureText: false,
+                labelText: 'Name'),
+            const SizedBox(
+              height: 20,
+            ),    
+            MytextField(
+                controller: emailController,
+                hintText: 'Enter your email',
+                obscureText: false,
+                labelText: 'Email'),
+            const SizedBox(
+              height: 20,
+            ),    
+            MytextField(
+                controller: passwordController,
+                hintText: 'Enter your password',
+                obscureText: true,
+                labelText: 'password'),
+            const SizedBox(
+              height: 20,
+            ),    
+            MytextField(
+                controller: repasswordController,
+                hintText: 'Enter your password again',
+                obscureText: true,
+                labelText: 'Re-password'),
+            const SizedBox(
+              height: 20,
+            ),
+        ],
+      )),
+    );
+  }
+}
